440e37536caf6d1c270616c123583a6ae619e0cc
colinmolina13 <colinmolina@mail.adelphi.edu>
2025-10-09 17:19:22 -0400

    Add files via upload

---
+++ src/components/ui/DraftClasses.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+const draftYears = [
+  "2025",
+  "2024",
+  "2023",
+  "2022",
+  "2021",
+  "2020",
+  "2019",
+  "2018",
+  "2017",
+];
+
+export default function DraftClasses() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredYears = draftYears.filter((year) =>
+    year.includes(searchTerm.trim())
+  );
+
+  return (
+    <div>
+      <div className="flex justify-center mb-5">
+        <input
+          type="text"
+          placeholder="Find a draft class..."
+          className="w-1/2 px-3 py-2 border border-gray-300 rounded-md"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
+      <main className="p-5 rounded-xl shadow-md max-w-4xl mx-auto">
+        <h2 className="text-2xl text-center mb-4">Draft Classes</h2>
+        <div className="flex flex-col gap-3">
+          {filteredYears.length > 0 ? (
+            filteredYears.map((year) => (
+              <div
+                key={year}
+                className="flex flex-wrap justify-between items-center gap-2"
+              >
+                <span className="text-lg font-bold">{year}</span>
+                <div className="flex flex-wrap gap-2.5 justify-end">
+                  <Link href={`/drafts/${year}`}>
+                    <button className="btn btn-soft">View Class</button>
+                  </Link>
+                  <button className="btn btn-soft">View re-drafts</button>
+                  <button className="btn btn-soft">Create Re-draft</button>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p className="text-center italic">No draft classes found.</p>
+          )}
+          <div>
+            <span className="italic">etc.</span>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
